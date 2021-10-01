d1de13acd3925a67713e35d0ebde5e583a415a8a
Sujit Kumar Behera <64631566+s-k3278@users.noreply.github.com>
2021-10-01 15:05:06 +0530

    Add files via upload
    Odd Even program by sujit

---
+++ oddEven_sujit.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+def nextnum(num):
+    if num%2 == 0:
+        print("even")
+    else:
+        print("odd")
+
+nextnum(67)
+
+
+# In[ ]:
+
+
+
+
